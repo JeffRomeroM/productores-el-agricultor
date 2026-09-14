19f8e6fcda9e1c9a9ae2b115e1e6e6522ee778f4
Jeffrin Romero <jeffrinromerom@gmail.com>
2026-09-14 13:31:37 -0600

    icono modificado

--- vite.config.js
+++ vite.config.js
@@ -27,7 +27,7 @@ export default defineConfig({
             type: 'image/png'
           },
           {
-            src: '/logo.png',
+            src: '/logoblanco.png',
             sizes: '512x512',
             type: 'image/png',
             purpose: 'any maskable'
